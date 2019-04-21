4dbeeb1706453f4dbbba11abe9d4b18dd8af3edb
mccraebears <mccrae@berkeley.edu>
2019-04-21 16:29:43 -0700

    Working on mask rcnn

--- mask_rcnn_testing.py
+++ mask_rcnn_testing.py
@@ -1,9 +1,11 @@
 from matplotlib import pyplot as plt
 from gluoncv import model_zoo, data, utils
 from PIL import Image
+import sys
 
 net = model_zoo.get_model('mask_rcnn_resnet50_v1b_coco', pretrained=True)
-x, orig_img = data.transforms.presets.rcnn.load_test("y/test.jpg")
+threshold = 0.9
+x, orig_img = data.transforms.presets.rcnn.load_test("{}".format(sys.argv[1]))
 
 ids, scores, bboxes, masks = [xx[0].asnumpy() for xx in net(x)]
 
@@ -11,6 +13,11 @@ ids, scores, bboxes, masks = [xx[0].asnumpy() for xx in net(x)]
 width, height = orig_img.shape[1], orig_img.shape[0]
 masks = utils.viz.expand_mask(masks, bboxes, (width, height), scores)
 orig_img = utils.viz.plot_mask(orig_img, masks)
+
+filtered_ids = [x for x in ids if x > threshold]
+class_names = net.classes
+
+print(set([class_names[item[0]] for item in filtered_ids]))
 
 # identical to Faster RCNN object detection
 fig = plt.figure(figsize=(10, 10))
