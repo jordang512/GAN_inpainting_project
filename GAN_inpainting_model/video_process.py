91d984e3916ea30db03da7473a7424135e4e8120
mccraebears <mccrae@berkeley.edu>
2019-05-07 15:25:44 -0700

    Still working on video (gif), but it is not working

--- GAN_inpainting_model/video_process.py
+++ GAN_inpainting_model/video_process.py
@@ -13,7 +13,7 @@ import tensorflow as tf
 import neuralgym as ng
 from inpaint_model import InpaintCAModel
 
-fname = 'ball.gif'
+fname = 'baseballshort.gif'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--image', default='', type=str,
@@ -89,37 +89,30 @@ def test_model(image, mask, output_dir='output_images/output.png', checkpoint_di
         result = sess.run(output)
         # cv2.imwrite(output_dir, result[0][:, :, ::-1])
         # plt.imsave('out.jpg', result[0][:, :, ::-1])
-        return result[0][:, :, ::-1]
+        return result[0]
 
 
 
 
 
-def get_masks(x, orig_img):
-    net = model_zoo.get_model('mask_rcnn_resnet50_v1b_coco', pretrained=True)
+def get_masks(x, orig_img, net, class_to_remove):
     threshold = 0.5
     #x, orig_img = data.transforms.presets.rcnn.transform_test(image)
     ids, scores, bboxes, masks = [xx[0].asnumpy() for xx in net(x)]
 
     # x is index, int(y[0]) is category id
     filtered_ids = np.array([(x,int(y[0])) for x,y in enumerate(ids) if scores[x] > threshold])
-    class_names = net.classes
 
-    # Prompt user to select a category
-    print("I found these categories: ")
-    unique_classes = list(set([class_names[item[1]] for item in filtered_ids]))
-    for idx,item in enumerate(unique_classes):
-        print("{}: {}".format(idx,item))
-    print("Please select one category by entering the number next to it")
-    # To get the category id, convert input->class->index of class
-    selection = net.classes.index(unique_classes[int(input("My choice is: "))])
 
     # Prune scores, masks, boxes, and ids by selection
     # It's important to define these as np.array's
-    scores = np.array([scores[item[0]] for item in filtered_ids if item[1]==selection])
-    masks = np.array([masks[item[0]] for item in filtered_ids if item[1]==selection])
-    bboxes = np.array([bboxes[item[0]] for item in filtered_ids if item[1]==selection])
-    ids = np.array([item[1] for item in filtered_ids if item[1]==selection])
+    scores = np.array([scores[item[0]] for item in filtered_ids if item[1]==class_to_remove])
+    masks = np.array([masks[item[0]] for item in filtered_ids if item[1]==class_to_remove])
+    bboxes = np.array([bboxes[item[0]] for item in filtered_ids if item[1]==class_to_remove])
+    ids = np.array([item[1] for item in filtered_ids if item[1]==class_to_remove])
+
+    if not masks:
+        return []
 
     width, height = orig_img.shape[1], orig_img.shape[0]
     masks = utils.viz.expand_mask(masks, bboxes, (width, height), scores)
@@ -137,14 +130,23 @@ def process_video(fname):
         frames.append((frame, orig_im))
 
     finished_frames = []
-    for frame in frames:
-        painted = process_frame(frame)
+    net = model_zoo.get_model('mask_rcnn_resnet50_v1b_coco', pretrained=True)
+    print([(x,y) for (x,y) in enumerate(net.classes)])
+    class_to_remove = input("Please enter a class index: ")
+
+    print(len(frames))
+
+    for count, frame in enumerate(frames):
+        painted = process_frame(frame, net, class_to_remove)
         finished_frames.append(painted)
+        print("Finished frame {}".format(count))
     imageio.mimsave('outgif.gif',frames)
 
 
-def process_frame(frame):
-    masks = get_masks(frame[0], frame[1])
+def process_frame(frame, net, class_to_remove):
+    masks = get_masks(frame[0], frame[1], net, class_to_remove)
+    if masks == []:
+        return frame[1]
     if masks.ndim == 3:
         compiled_mask = np.amax(masks, axis=0)
     else:
